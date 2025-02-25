3dc8a7417f81684a6d3903c10ddd28100967f3a1
Nefta11 <neftaliarturohernandez@gmail.com>
2025-02-25 11:32:31 -0600

    Agregar navegación interactiva al componente Carousel con funciones para avanzar y retroceder diapositivas

--- src/components/CarouselComponent.vue
+++ src/components/CarouselComponent.vue
@@ -4,10 +4,10 @@
     <!-- Navegación -->
     <div class="navigate">
       <div class="toggle-page left">
-        <i class="fas fa-chevron-left" />
+        <i @click="prevSlide" class="fas fa-chevron-left"></i>
       </div>
-      <div class="toggle-page right">
-        <i class="fas fa-chevron-right" />
+      <div @click="nextSlide" class="toggle-page right">
+        <i class="fas fa-chevron-right"></i>
       </div>
     </div>
   </div>
@@ -21,16 +21,34 @@ export default {
     const currentSlide = ref(1);
     const getSlideCount = ref(0);
 
+    // Siguiente Slide
+    const nextSlide = () => {
+      if (currentSlide.value === getSlideCount.value) {
+        currentSlide.value = 1;
+        return;
+      }
+      currentSlide.value += 1;
+    };
+
+    // Slide Anterior
+    const prevSlide = () => {
+      if (currentSlide.value === 1) {
+        currentSlide.value = getSlideCount.value;
+        return;
+      }
+      currentSlide.value -= 1;
+    };
+
     onMounted(() => {
-      getSlideCount.value = document.querySelectorAll('.slide').length;
-      console.log('Slide count:', getSlideCount.value);
+      getSlideCount.value = document.querySelectorAll(".slide").length;
+      console.log("Slide count:", getSlideCount.value);
       // Simulación de cambio de diapositiva
       setInterval(() => {
         currentSlide.value = (currentSlide.value % getSlideCount.value) + 1;
       }, 3000);
     });
 
-    return { currentSlide, getSlideCount };
+    return { currentSlide, getSlideCount, nextSlide, prevSlide };
   },
 };
 </script>
